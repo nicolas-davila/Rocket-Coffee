b9270bd044a7a32aa59b893f6910fa1f27220dce
Nicolas Dávila <123404361+nicolas-davila@users.noreply.github.com>
2023-02-27 19:30:18 +0000

    atualização final do rocket coffee. concluido!

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function clicou () {
   const textoSumir = document.querySelector('.letreiro')
   const listaAberta = document.querySelector('.lista')
   const footerSumir = document.querySelector('footer')
+  const sumirCoffee = document.querySelector('.coffee-image')
     
   if(botaoClose.innerHTML === `<img src="./imagens/menu-buguer-close.svg">`) {
     botaoClose.innerHTML = ''
@@ -14,4 +15,5 @@ function clicou () {
   textoSumir.classList.toggle('sumir-texto')
   listaAberta.classList.toggle('lista-aberta')
   footerSumir.classList.toggle('footer-sumir')
+  sumirCoffee.classList.toggle('coffee-sumir');
 }
